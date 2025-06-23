fba184b86124011fe60d7d91f1626be8f466b529
synoize <suryavanshivam@gamil.com>
2025-06-23 21:35:43 +0530

    project deploy

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ import userRouter from './routes/user-route.js';
 const app = express();
 const PORT = process.env.PORT || 4000
 const corsOptions = {
-    origin: ["https://neardr.onrender.com", "https://neardr-admin.onrender.com"],
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
 };
@@ -26,7 +26,6 @@ app.use(cors(corsOptions))
 app.use('/api/admin', adminRouter)
 app.use('/api/doctor', doctorRouter)
 app.use('/api/user', userRouter)
-
 
 
 app.get('/', (req, res) => {
